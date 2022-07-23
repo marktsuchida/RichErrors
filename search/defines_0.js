29eea28b87037d97e23c7de7639e49da8f20cb53
marktsuchida <marktsuchida@users.noreply.github.com>
2022-07-23 19:08:59 +0000

    deploy: b64e7f0da55fbbb1e155d62b595e3bb2f2e73899

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,5 +1,12 @@
 var searchData=
 [
+  ['rerr_5fcodeformat_5fhex16_379',['RERR_CodeFormat_Hex16',['../RichErrors_8h.html#afabcaf9bd4608d8f22b1a9ef9bec7fd4',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fhex32_380',['RERR_CodeFormat_Hex32',['../RichErrors_8h.html#aa4807b37030f3cf9dca87b6083888520',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fhexnopad_381',['RERR_CodeFormat_HexNoPad',['../RichErrors_8h.html#aa90021c337db68c5d97b963b6c47fedb',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fi16_382',['RERR_CodeFormat_I16',['../RichErrors_8h.html#a4367aa969a00be40062c83ac60408710',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fi32_383',['RERR_CodeFormat_I32',['../RichErrors_8h.html#a341bf93ac02d27a2b2b4f20b5fd3c1a6',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fu16_384',['RERR_CodeFormat_U16',['../RichErrors_8h.html#a0c753e862ef6a2be7414b4fcf9f5bd24',1,'RichErrors.h']]],
+  ['rerr_5fcodeformat_5fu32_385',['RERR_CodeFormat_U32',['../RichErrors_8h.html#aef88864f2090da126d2f036a8adad64a',1,'RichErrors.h']]],
   ['rerr_5fdomain_5fcritical_386',['RERR_DOMAIN_CRITICAL',['../RichErrors_8h.html#a48525879100ce626ee90686434923d21',1,'RichErrors.h']]],
   ['rerr_5fdomain_5fricherrors_387',['RERR_DOMAIN_RICHERRORS',['../RichErrors_8h.html#a39e5acf869f4f7a6917f187ec36cb66b',1,'RichErrors.h']]],
   ['rerr_5fformatted_5fcode_5fmax_5flen_388',['RERR_FORMATTED_CODE_MAX_LEN',['../RichErrors_8h.html#a1e153d9e9ad2bf21e182e13bcdf6d082',1,'RichErrors.h']]],
